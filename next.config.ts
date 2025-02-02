137e21497041b2f9682604d26f42eccddb63a3c2
Battseren Badral <bbattseren88@gmail.com>
2025-02-02 13:52:50 +0100

    Github workflows

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/mongolian-passport",
+  output: "export"
 };
 
 export default nextConfig;
