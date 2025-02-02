a6e42cc19e972ca0e8225fd58bf3452d5b1024a2
Battseren Badral <bbattseren88@gmail.com>
2025-02-02 18:00:14 +0100

    Next initial

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/mongolian-passport",
-  output: "export"
+  distDir: "docs",
 };
 
 export default nextConfig;
