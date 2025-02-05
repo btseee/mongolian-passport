b16d80c450c137cb2b1eb223f3abdbb450cb7a72
Battseren Badral <bbattseren88@gmail.com>
2025-02-05 22:41:17 +0100

    Add GitHub Actions workflow for publishing to GitHub Pages and configure Next.js for static export

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    basePath: "/mongolian-passport",
+    output: "export",
     images:{
         domains: ['flagcdn.com']
     }
